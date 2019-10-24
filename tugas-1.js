5f21d2d0b2999edb0a732023114882c6f0eedb76
bamboo110 <9bbambangsetyo.b@gmail.com>
2019-10-24 20:37:20 +0700

    update

--- tugas-1.js
+++ tugas-1.js
@@ -1,9 +1,9 @@
 function number(a) {
-  let b = '';
-  for(let x = 0; x < a; x++){
-    b += x + 1;
-  }
   if (Number.isInteger(a)) {
+    let b = '';
+    for(let x = 0; x < a; x++){
+      b += x + 1;
+    }
     console.log(b);
   } else {
     console.log('tolong masukan angka')
